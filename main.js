f0eefca7444a230ffa4115071027678710948240
dkisic <32257921+dkisic@users.noreply.github.com>
2019-03-20 16:37:12 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,47 +8,44 @@
 function kendoGridDataBound(e) {
     var grid = e.sender;
     var columns = grid.columns;
+    var column;
 
-    //checking if grid is grouped any by how many columns
+    //checking if grid is grouped and by how many columns
     var numItems = grid.dataSource.group().length;
+
+    //checking if grid is hierarchical and appending value (1 if yes, 0 if no)
+    var hierarchicalGridNum = grid.tbody.find("tr[role='row'].k-master-row").length > 0 ? 1 : 0;
 
     //selecting all grid cells
     var cells = grid.tbody.find("tr[role='row']").not(".k-grouping-row").find("td");
 
-    //checking if grid is hierarchical and appending value (1 if yes, 0 if no)
-    var hierarchicalGridNum = grid.tbody.find("tr[role='row'].k-master-row").length > 0 ? 1 : 0;
-    var currentCell, column;
-
     //iterating through grid cells
-    for (var i = 0; i < cells.length; i++) {
-        currentCell = $(cells[i]);
-        //getting the column in which cell belongs
-        //if grid is grouped or hierachical the cells are appended to the beginning of row 
-        //so we take away numItems(nr of groups) and hierarchicalGridNum(hierarchical grid ?) from cell index
-        column = columns[currentCell.index() - numItems - hierarchicalGridNum];
+    cells.each(function (index) {
+        //if grid is grouped or hierachical we subtract numItems and hierarchicalGridNum from current cell index
+        //we do it because kendo appends extra cells to beginning of every row if the grid is grouped or hierarchical
+        //and then we get the column that current cell really belongs to
+        column = columns[$(this).index() - numItems - hierarchicalGridNum];
 
         if (column) {
-            //checking that title doesn't match '&nbsp;'
-            if (column.title !== '&nbsp;') {
+            if (column.title !== '&nbsp') {
                 //adding data-title attribute to cell with value of column title
-                currentCell.attr("data-title", column.title);
+                $(this).attr("data-title", column.title);
             }
         }
-    }
+    });
 
-    //iterating through footer cells
+    //handling footer
     var footerCells = $(".k-footer-template td").not(".k-group-cell");
-    var currentFooterCell;
 
-    for (var j = 0; j < footerCells.length; j++) {
-        currentFooterCell = $(footerCells[j]);
-        column = columns[currentFooterCell.index() - numItems];
+    //we do the same thing for footer
+    footerCells.each(function (index) {
+        column = columns[$(this).index() - numItems];
 
         if (column) {
-            currentFooterCell.attr("data-title", column.title + " (" + abp.localization.localize("Total", "BI") + ")");
-            hideNbsp(currentFooterCell);
+            $(this).attr("data-title", column.title + " (" + abp.localization.localize("Total", "BI") + ")");
+            hideNbsp($(this));
         }
-    }
+    });
 }
 
 function setKendoGridsForMobileDisplay() {
